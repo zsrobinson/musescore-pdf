1bbe32a0884299418052b95614de1d22ebfe5968
Zachary Robinson <me@zsrobinson.com>
2023-07-23 01:05:26 -0400

    fix scaling issue

--- lib/url-to-pdf.ts
+++ lib/url-to-pdf.ts
@@ -7,5 +7,16 @@ export async function urlToPDF(url: string) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setContent(svg);
-  return await page.pdf({ format: "A4", scale: 0.3 });
+
+  const width =
+    (await page.evaluate(() => {
+      const svg = document.querySelector("svg");
+      return svg?.getAttribute("width");
+    })) ?? "3000";
+
+  // 0.3 scale works well for 2976.38
+  // therefore scale should be 0.3 * (2976.38 / width) => 900 / width
+  // not sure where the 900 comes from, but it works
+
+  return await page.pdf({ format: "A4", scale: 900 / Number(width) });
 }
