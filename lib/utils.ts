33b17bddc8717ac0cddd4ed252e9b32203468784
Zachary Robinson <me@zsrobinson.com>
2023-07-23 22:17:04 -0400

    add shadcn/ui

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,6 @@
-// https://stackoverflow.com/a/9458996/15938350
-export function arrayBufferToBase64(buffer: ArrayBuffer) {
-  let binary = "";
-  let bytes = new Uint8Array(buffer);
-  let len = bytes.byteLength;
-  for (let i = 0; i < len; i++) {
-    binary += String.fromCharCode(bytes[i]);
-  }
-  return btoa(binary);
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
